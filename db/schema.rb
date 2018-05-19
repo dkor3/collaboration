808b617cd8788219711f89bba1b7c2adfa68ce9a
qlqnf16 <lee.jeongmin1027@likelion.org>
2018-05-20 06:24:29 +0900

    예약하러가기 라우팅 생성(DB수정있음)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180519181549) do
+ActiveRecord::Schema.define(version: 20180519212035) do
 
   create_table "books", force: :cascade do |t|
     t.text     "name"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20180519181549) do
     t.string   "subway"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "writer"
   end
 
   create_table "users", force: :cascade do |t|
